986387d9e29d2d80e970fe8ad7cebfd41bf43b4e
jhs512 <jangka512@gmail.com>
2025-01-14 09:38:14 +0900

    work

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import ClientLayout from "./ClientLayout";
 import "./globals.css";
-import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,22 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-[100dvh] border-[5px]`}
       >
-        <header className="border-[2px] border-[red] p-5">
-          <div className="flex gap-2">
-            <Link href="/">홈</Link>
-            <Link href="/about">소개</Link>
-            <Link href="/post/list">글</Link>
-            <Link href="/member/login">로그인</Link>
-          </div>
-        </header>
-
-        <main className="flex-grow border-[2px] border-[blue] p-5">
-          {children}
-        </main>
-
-        <footer className="border-[2px] border-[pink] p-5">
-          Copyright 2025.
-        </footer>
+        <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
   );
